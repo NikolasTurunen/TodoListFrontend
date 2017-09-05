6a1a6c8ef46e34198f9419076c2789beb855add6
Nikolas Turunen <nikoltur@gmail.com>
2017-09-05 13:50:22 +0300

    Added text inputs of dialogs to be focused when opened

--- public_html/app.js
+++ public_html/app.js
@@ -1,3 +1,16 @@
 angular.module("app", [
     "ngResource"
 ]);
+
+angular.module("app").directive('focusMe', function () {
+    return {
+        link: function (scope, element, attrs) {
+            scope.$watch(attrs.focusMe, function (value) {
+                if (value === true) {
+                    element[0].focus();
+                    element[0].select();
+                }
+            });
+        }
+    };
+});
